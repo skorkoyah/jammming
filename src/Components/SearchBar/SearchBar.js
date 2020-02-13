73c13f0130ba71ac2e1c63f38eecd0de2e8cad16
skorkoyah <skorkoyah29@gmail.com>
2020-02-13 00:37:37 -0500

    uploading project files

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    };
+    
+  }
+  
+  // Sets the argument for the seearch method in App.js to the state of term
+  search = (event) => {
+    this.props.onSearch(this.state.term);
+  }
+  
+  // Sets the state of term to the value the user enters in input
+  handleTermChange = (event) => {
+    this.setState({ term: event.target.value });
+  }
+
+  handleKeyPress = (event) => {
+    if(event.key === 'Enter') {
+      this.search();
+    }
+  }
+  
+  render() {
+    return(
+      <div className="SearchBar">
+        <input  placeholder="Enter A Song, Album, or Artist"
+                onChange={this.handleTermChange}
+                onKeyPress={this.handleKeyPress} />
+        <a  onClick={this.search}>SEARCH</a>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
